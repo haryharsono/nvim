6ce4e39445f259e548f09e47f69cc2ccb7bbaecb
haryharsono <hary.harsono@ist.id>
2026-02-22 12:15:18 +0700

    Add neo-tree expand/collapse all shortcuts

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      mappings = {
+        ["E"] = "expand_all_nodes",
+        ["W"] = "close_all_nodes",
+      },
+    },
+  },
+}
