7c75e7adf66c85d22b02d63ffab44d0f052377f0
haryharsono <hary.harsono@ist.id>
2026-02-22 10:33:40 +0700

    Add Leader+fn shortcut to create new file

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -79,6 +77,18 @@ return {
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
+
+        -- Create new file with prompt
+        ["<Leader>fn"] = {
+          function()
+            vim.ui.input({ prompt = "New file: " }, function(name)
+              if name and name ~= "" then
+                vim.cmd("e " .. name)
+              end
+            end)
+          end,
+          desc = "New file",
+        },
       },
     },
   },
